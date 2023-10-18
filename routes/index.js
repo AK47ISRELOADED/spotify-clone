0900a9adda5bab2a69c20cd7bfa012e2bc85c0a6
AK47ISRELOADED <adarshkaurav47@gmail.com>
2023-10-18 11:12:53 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
 
-var users = require("/models/userModel");
+
+var users = require('../models/userModel')
+var songModel = require('../models/songModel')
+var playlistModel = require('../models/playlistModel')
 var passport = require("passport");
 var localStrategy = require('passport-local');
-const { default: mongoose } = require('mongoose');
+var id3 = require('node-id3');
+
 passport.use(new localStrategy(users.authenticate()));
 
+const mongoose = require('mongoose');
+const songModel = require('../models/songModel');
 
+mongoose.connect('mongodb://0.0.0.0/spotify').then(() => {
+  console.log('connected to database')
+}).catch(err => {
+  console.log(err)
+})
+
+const conn = mongoose.connection
+
+var gfsBucket, gfsBucketPoster
+conn.once('open', () => {
+  gfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
+    bucketName: 'audio'
+  })
+  gfsBucketPoster = new mongoose.mongo.GridFSBucket(conn.db, {
+    bucketName: 'poster'
+  })
+})
+
+
+
+
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+
+router.post('/uploadMusic',upload.single('song') , async (req,res,next)=>{
+
+})
+
+
+
+
+//---------------------------------------------------------------------
 
 // registering a user
 
@@ -56,3 +96,7 @@ function isLoggedIn(req,res,next){
         res.redirect('/login');
     }
 }
+
+
+//--------------------------------------------------------------------
+
